a56a4bf4c80f16ff8b4dcfef59ebb0f866c0a95a
Erik Khalimov <biomaks@gmail.com>
2015-07-04 14:47:55 +0300

    changed configs and added configs for docker

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:2.7
+MAINTAINER Erik Khalimov <erik.khalimov@me.com>
+RUN apt-get update && apt-get install -y curl postgresql-client vim nano 
+WORKDIR /application
+ADD application /application
+RUN pip install -r requirements.txt
+EXPOSE 5000
--- config.py
+++ config.py
@@ -5,7 +5,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = False
 TESTING = False
 
+DB_HOST = os.getenv('DB_PORT_5432_TCP_ADDR')
+DB_PORT = os.getenv('DB_PORT_5432_TCP_PORT')
+
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
-SQLALCHEMY_DATABASE_URI = 'postgresql://dr_quiz_db:dr_quiz_db@localhost:5432/dr_quiz_db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://dr_quiz_db:dr_quiz_db@'+DB_HOST+':'+DB_PORT+'/dr_quiz_db'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
