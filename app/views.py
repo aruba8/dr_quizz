4306bb3c17544097f49a9b9439c6e69ad1c048bd
Erik Khalimov <biomaks@gmail.com>
2015-07-05 10:52:52 +0300

    added tests and some fixes

--- app/views.py
+++ app/views.py
@@ -19,7 +19,9 @@ def index():
 class QuestionList(Resource):
     def get(self, question_number):
         question_list = []
-        questions = self.get_questions(question_number)
+        questions_count = self.get_questions_count()
+        questions_fromdb = self.get_questions_from_db()
+        questions = self.get_questions(question_number, questions_count, questions_fromdb)
         for question in questions:
             question_list.append(self.add_answers(question))
         return json.dumps({"questions": question_list})
@@ -40,20 +42,27 @@ class QuestionList(Resource):
 
         return question_to_return
 
-    def get_questions(self, number):
-        questions = []
+    def get_questions(self, number, questions_count, questions_fromdb):
         signed_qty, usual_qty = self.define_qty(number)
-        signed_count = Question.query.filter_by(signed=True).count()
-        usual_count = Question.query.filter_by(signed=False).count()
-        signed_questions = Question.query.filter_by(signed=True).all()
-        usual_questions = Question.query.filter_by(signed=False).all()
-        random_numbers_signed = random.sample(range(1, signed_count+1), signed_qty)
-        random_numbers_usual = random.sample(range(1, usual_count+1), usual_qty)
+        signed_count, usual_count = questions_count
+        signed_questions, usual_questions = questions_fromdb
+        random_numbers_signed = self.generate_random_numbers(signed_count, signed_qty)
+        random_numbers_usual = self.generate_random_numbers(usual_count, usual_qty)
+        questions = []
         for number in random_numbers_usual:
             questions.append(usual_questions[number - 1])
         for number in random_numbers_signed:
             questions.append(signed_questions[number - 1])
         return questions
+
+    def generate_random_numbers(self, count, qty):
+        return random.sample(range(1, count + 1), qty)
+
+    def get_questions_count(self):
+        return Question.query.filter_by(signed=True).count(), Question.query.filter_by(signed=False).count()
+
+    def get_questions_from_db(self):
+        return Question.query.filter_by(signed=True).all(), Question.query.filter_by(signed=False).all()
 
     def define_qty(self, number):
         signed_qty, usual_qty = 0, 0
--- tests.py
+++ tests.py
@@ -1,10 +1,22 @@
 import unittest
 from app.views import QuestionList, Check
+from app.models import Question, Answer
 
 class TestQuestionList(unittest.TestCase):
 
     def setUp(self):
         self.question_list = QuestionList()
+
+    def prepare_questions(self):
+        questions_unsigned = []
+        questions_signed = []
+        for i in range(100):
+            q_unsigned = Question(id=i, real_id=i, signed=False, question='This is question '+str(i))
+            questions_unsigned.append(q_unsigned)
+        for i in range(100):
+            q_signed = Question(id=i, real_id=i, signed=True, sign_name='TestSignName'+str(i), question='This is question '+str(i))
+            questions_signed.append(q_signed)
+        return questions_signed, questions_unsigned
 
     def test_define_qty(self):
         defined5 = self.question_list.define_qty(5)
@@ -15,6 +27,19 @@ class TestQuestionList(unittest.TestCase):
         self.assertTrue(defined10[0] == 4 and defined10 [1] == 6)
         self.assertTrue(defined20[0] == 7 and defined20 [1] == 13)
         self.assertTrue(defined40[0] == 10 and defined40 [1] == 30)
+
+    def test_generate_random_numbers(self):
+        count = 100
+        qty = 10
+        random_list = self.question_list.generate_random_numbers(count, qty)
+        self.assertTrue(type(random_list) is list)
+        self.assertEquals(len(random_list), 10)
+
+    def test_get_questions(self):
+        prepared_questions = self.prepare_questions()
+        question_list = self.question_list.get_questions(20, (100,100), prepared_questions)
+        self.assertEquals(len(question_list), 20)
+        self.assertIsInstance(question_list[10], Question)
 
 class TestCheck(unittest.TestCase):
 
