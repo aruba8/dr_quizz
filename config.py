15d6d13f2155f6a2c422cbd552e8cf2ce8173c74
Erik Khalimov <biomaks@gmail.com>
2015-08-15 19:16:51 +0300

    moved to sqllite

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ DB_PORT = os.getenv('DB_PORT_5432_TCP_PORT')
 
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
-SQLALCHEMY_DATABASE_URI = 'postgresql://dr_quiz_db:dr_quiz_db@'+DB_HOST+':'+DB_PORT+'/dr_quiz_db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'dr_quiz.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
