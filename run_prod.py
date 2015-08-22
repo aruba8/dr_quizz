d5dd25aec978689363397a1e6e0002d2a3219464
Erik Khalimov <biomaks@gmail.com>
2015-08-22 14:13:49 +0300

    moved to rocket added missed file for angular

--- run_prod.py
+++ run_prod.py
@@ -1,11 +1,18 @@
-from cherrypy import wsgiserver
-from app import app
+from rocket import Rocket
 
-d = wsgiserver.WSGIPathInfoDispatcher({'/': app})
-server = wsgiserver.CherryPyWSGIServer(('0.0.0.0', 5000), d)
+from app import app
+import logging
+
+
+def run_server():
+    log = logging.getLogger('Rocket')
+    log.setLevel(logging.INFO)
+    log.addHandler(logging.FileHandler('dr_quizz.log'))
+    server = Rocket(interfaces=('0.0.0.0', 5000),
+                    method='wsgi',
+                    app_info={"wsgi_app": app})
+    server.start()
+
 
 if __name__ == '__main__':
-    try:
-        server.start()
-    except KeyboardInterrupt:
-        server.stop()
+    run_server()
